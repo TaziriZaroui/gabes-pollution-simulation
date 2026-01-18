6274a3572b1370294a3418b6fbc326b5fa25bc1b
TaziriX <99590545+TaziriZaroui@users.noreply.github.com>
2026-01-18 15:46:54 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -62,4 +62,5 @@ df.to_csv('resultats_gabes.csv', index=False)
 print("\n✅ Fichiers créés : gabes_result.png et resultats_gabes.csv")
 
 *result*
-![Graphique résultats]
+![Graphique résultats](Capture d'écran 2026-01-18 154038.png)
+(Capture d'écran 2026-01-18 154027.png  )
